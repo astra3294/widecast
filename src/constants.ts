703733b172c66eca4422e2a7fe46c4770372b687
astra3294 <astra3294@users.noreply.github.com>
2026-08-16 03:40:25 +0800

    chore: 0.1.5

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const WIDECAST_VERSION = '0.1.4'
+export const WIDECAST_VERSION = '0.1.5'
 export const SUPPORTED_DSH_VERSION = '0.1.0-rc.6'
 export const RPC_CHANNEL = '/widecast'
 export const DEFAULT_PROFILE = 'web'
